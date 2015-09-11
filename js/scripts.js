eb0e02360c703f88cf2ba0786d61eb8407888bb3
AlexAlt <alexandra.m.altieri@gmail.com>
2015-09-11 11:13:22 -0700

    Handles special cases with upper/lowercase first letters

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 var findAndReplace = function(theString, foundWord, replacementWord) {
-  return theString.replace(foundWord, replacementWord);
-}
+  var lowerFoundWord = foundWord.toLowerCase();
+  var lowerReplacementWord = replacementWord.toLowerCase();
+
+  var upperFoundWord = foundWord.charAt(0).toUpperCase() + foundWord.slice(1);
+  var upperReplacementWord = replacementWord.charAt(0).toUpperCase() + replacementWord.slice(1);
+
+  if (theString.search(lowerFoundWord) >= 0) {
+    return theString.replace(lowerFoundWord, lowerReplacementWord);
+  } else if (theString.search(upperFoundWord) >= 0) {
+    return theString.replace(upperFoundWord, upperReplacementWord);
+  }
+
+};
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,22 @@ describe('findAndReplace', function() {
   it('replaces a word within a string', function() {
     expect(findAndReplace("I love dogs", "dogs", "cats")).to.equal("I love cats");
   });
+
+  it('handles special cases where replacement words are uppercase', function() {
+    expect(findAndReplace("I love dogs", "Dogs", "Cats")).to.equal("I love cats");
+  });
+
+  it('handles special cases where replacement words are lowercase', function() {
+    expect(findAndReplace("I love Seattle", "seattle", "portland")).to.equal("I love Portland");
+  });
+
+  it('handles special cases where words have mis-matched cases', function() {
+    expect(findAndReplace("I love Seattle", "Seattle", "portland")).to.equal("I love Portland");
+    expect(findAndReplace("I love dogs", "dogs", "Cats")).to.equal("I love cats");
+  });
+
+  it('handles special cases where words have mis-matched cases', function() {
+    expect(findAndReplace("I love Seattle", "SeaTTle", "Portland")).to.equal("I love Portland");
+  });
+
 });
