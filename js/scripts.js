6cecd6f47077c582cce0cf65dad693f8e0dde3df
AlexAlt <alexandra.m.altieri@gmail.com>
2015-09-11 12:37:35 -0700

    Change code for refactoring and different spec purposes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,70 @@
 var findAndReplace = function(theString, foundWord, replacementWord) {
-  var foundPush = [];
 
-  var foundLetters = foundWord.slice(1).split("");
-  foundLetters.forEach(function(letter) {
-    var lowLetter = letter.toLowerCase();
-    foundPush.push(lowLetter);
-  }); 
-  var formattedFoundWord = foundWord.charAt(0) + foundPush.join("");
+  var stringWords = theString.split(" ");
+  var result = [];
+  
+  stringWords.forEach(function(word) {
+    if (word.toLowerCase() === foundWord.toLowerCase()) {
+      result.push(replacementWord);
+    } else {
+      result.push(word);
+    }
+   });
 
-  var replacePush = [];
+  return result.join(" ");
+};
 
-  var replaceLetters = replacementWord.slice(1).split("");
-  replaceLetters.forEach(function(letter) {
-    var lowReplace = letter.toLowerCase();
-    replacePush.push(lowReplace);
-  });
-  var formattedReplaceWord = replacementWord.charAt(0) + replacePush.join("");
+  // var foundPush = [];
+  // var foundLetters = foundWord.slice(1).split("");
+  // foundLetters.forEach(function(letter) {
+  //   var lowLetter = letter.toLowerCase();
+  //   foundPush.push(lowLetter);
+  // }); 
+  // var formattedFoundWord = foundWord.charAt(0) + foundPush.join("");
+
+
+  // var replacePush = [];
+  // var replaceLetters = replacementWord.slice(1).split("");
+  // replaceLetters.forEach(function(letter) {
+  //   var lowReplace = letter.toLowerCase();
+  //   replacePush.push(lowReplace);
+  // });
+  // var formattedReplaceWord = replacementWord.charAt(0) + replacePush.join("");
   
 
-  var lowerFoundWord = formattedFoundWord.toLowerCase();
-  var lowerReplacementWord = formattedReplaceWord.toLowerCase();
+  // var lowerFoundWord = formattedFoundWord.toLowerCase();
+  // var lowerReplacementWord = formattedReplaceWord.toLowerCase();
 
-  var upperFoundWord = formattedFoundWord.charAt(0).toUpperCase() + formattedFoundWord.slice(1);
-  var upperReplacementWord = formattedReplaceWord.charAt(0).toUpperCase() + formattedReplaceWord.slice(1);
+  // var upperFoundWord = formattedFoundWord.charAt(0).toUpperCase() + formattedFoundWord.slice(1);
+  // var upperReplacementWord = formattedReplaceWord.charAt(0).toUpperCase() + formattedReplaceWord.slice(1);
 
 
-  if (theString.search(lowerFoundWord) >= 0) {
-    return theString.replace(lowerFoundWord, lowerReplacementWord);
-  } else if (theString.search(upperFoundWord) >= 0) {
-    return theString.replace(upperFoundWord, upperReplacementWord);
-  }
+  // if (theString.search(lowerFoundWord) >= 0) {
+  //   return theString.replace(lowerFoundWord, lowerReplacementWord);
+  // } else if (theString.search(upperFoundWord) >= 0) {
+  //   return theString.replace(upperFoundWord, upperReplacementWord);
+  // }
 
-};
+// };
+
+
+
+$(document).ready(function(){
+  $("form#find-replace").submit(function(event){
+    $("#result").hide();
+    var wordString= $("input#word-string").val();
+    var foundWord = $("input#found-word").val();
+    var replacementWord = $("input#replacement-word").val();
+    var result = findAndReplace(wordString, foundWord, replacementWord);
+
+   
+    $(".new-string").text(result)
+    $("#result").show();
+   
+    event.preventDefault();
+
+  })
+});
+
+
+
--- spec/specs.js
+++ spec/specs.js
@@ -3,29 +3,33 @@ describe('findAndReplace', function() {
     expect(findAndReplace("I love dogs", "dogs", "cats")).to.equal("I love cats");
   });
 
-  it('handles special cases where replacement words are uppercase', function() {
-    expect(findAndReplace("I love dogs", "Dogs", "Cats")).to.equal("I love cats");
-  });
+  // it('handles special cases where replacement words are uppercase', function() {
+  //   expect(findAndReplace("I love dogs", "Dogs", "Cats")).to.equal("I love cats");
+  // });
 
-  it('handles special cases where replacement words are lowercase', function() {
-    expect(findAndReplace("I love Seattle", "seattle", "portland")).to.equal("I love Portland");
-  });
+  // it('handles special cases where replacement words are lowercase', function() {
+  //   expect(findAndReplace("I love Seattle", "seattle", "portland")).to.equal("I love Portland");
+  // });
 
-  it('handles special cases where words have mis-matched cases', function() {
-    expect(findAndReplace("I love Seattle", "Seattle", "portland")).to.equal("I love Portland");
-    expect(findAndReplace("I love dogs", "dogs", "Cats")).to.equal("I love cats");
-  });
+  // it('handles special cases where words have mis-matched cases', function() {
+  //   expect(findAndReplace("I love Seattle", "Seattle", "portland")).to.equal("I love Portland");
+  //   expect(findAndReplace("I love dogs", "dogs", "Cats")).to.equal("I love cats");
+  // });
 
   it('handles special cases where words have mis-matched cases', function() {
     expect(findAndReplace("I love Seattle", "SeaTTle", "Portland")).to.equal("I love Portland");
   });
 
-  it('handles special cases where words have mis-matched cases', function() {
-    expect(findAndReplace("I love Seattle", "SeaTTle", "PortLAnd")).to.equal("I love Portland");
-  });
+  // it('handles special cases where words have mis-matched cases', function() {
+  //   expect(findAndReplace("I love Seattle", "SeaTTle", "PortLAnd")).to.equal("I love Portland");
+  // });
 
-   it('handles special cases where words have mis-matched cases', function() {
-    expect(findAndReplace("I love Portland", "I", "you")).to.equal("You love Portland");
+  //  it('handles special cases where words have mis-matched cases', function() {
+  //   expect(findAndReplace("I love Portland", "I", "you")).to.equal("You love Portland");
+  // });
+
+  it('handles special cases where words have mis-matched cases', function() {
+    expect(findAndReplace("I lOvE pOOp", "poop", "caRDboard")).to.equal("I lOvE caRDboard");
   });
 
 });
